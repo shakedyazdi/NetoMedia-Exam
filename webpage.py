c48e531b090fc81832c8da45aa0c873b94e95c94
shakedyazdi <shakedyaz18@gmail.com>
2021-04-21 15:26:21 +0300

    initial commit

---
+++ webpage.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from cpu_load_generator import load_all_cores
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def hello():
+    return "Hello World!"
+
+@app.route("/intense")
+def intense():
+    load_all_cores(duration_s=30, target_load=0.95)
+    return "Got You!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
